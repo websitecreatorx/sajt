09204def743be71b56b57c6fc65b3028544f7ddf
drip <118680430+websitecreatorx@users.noreply.github.com>
2023-09-20 21:21:54 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const red2 = document.querySelector('.red2');
 const sivo = document.querySelector('header');
 const sivo1 = document.querySelector('.red3')
+const Top = document.querySelector('.top')
 
 red2.addEventListener('click', function(){
     red2.classList.toggle('anim');
@@ -11,10 +12,30 @@ red2.addEventListener('click', function(){
 
 window.addEventListener('scroll', function()  {
     if (window.pageYOffset > 0){
-        sivo.classList.add('sivo');
-        sivo1.classList.add('sivo1');
+        sivo.classList.add('sivo')
+        sivo1.classList.add('sivo1')
     }else{
-        sivo.classList.remove('sivo');
-        sivo1.classList.remove('sivo1');
+        sivo.classList.remove('sivo')
+        sivo1.classList.remove('sivo1')
     }
 });
+
+window.addEventListener('scroll', function()  {
+    if (window.pageYOffset > 2036){
+        Top.classList.add('dugme')
+        Top.classList.remove('nedugme')
+    }else{
+        Top.classList.remove('dugme')
+        Top.classList.add('nedugme')
+    }
+    if(window.innerWidth < 768){
+        if (window.pageYOffset > 3640){
+            Top.classList.add('dugme')
+            Top.classList.remove('nedugme')
+        }else{
+            Top.classList.remove('dugme')
+            Top.classList.add('nedugme')
+        }    
+    }
+});
+
