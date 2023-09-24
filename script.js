57538002b27477ca990c023824f8e33f252a06a3
drip <118680430+websitecreatorx@users.noreply.github.com>
2023-09-24 21:30:28 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -2,9 +2,12 @@ const red2 = document.querySelector('.red2')
 const sivo = document.querySelector('header')
 const sivo1 = document.querySelector('.red3')
 const Top = document.querySelector('.top')
-const scrollableElement = document.querySelector('.wrapper')
+const scrollableElement = document.querySelector('.visiblecards')
+const scrollableElement1 = document.querySelector('.visiblecards1')
 const visina = document.querySelector('.crvenastrelica')
 const crveno = document.querySelector('.crvenirow')
+const dot1 = document.querySelector('.dot1')
+const dot2 = document.querySelector('.dot2')
 
 window.addEventListener('load', function(){
     setTimeout(scrollback, 0)
@@ -73,5 +76,49 @@ scrollableElement.addEventListener('scroll', function(){
 
 scrollableElement.addEventListener('scroll', function(){
     if(scrollableElement.scrollLeft === 0){
-        myinterval = setInterval(scroll, intervalTime)
+        myinterval = setInterval(scroll, 3000)
 }})
+
+
+const scrollAmount1 = scrollableElement1.offsetWidth + 100
+myinterval1 = setInterval(scroll1, 6000)
+
+function scroll1(){
+    scrollableElement1.scrollBy(scrollAmount1, intervalTime)
+    dot2.classList.add('activedot')
+    dot1.classList.remove('activedot')
+}
+
+function scrollback1(){
+    scrollableElement1.scrollLeft = scrollBack;
+    dot1.classList.add('activedot')
+    dot2.classList.remove('activedot')
+}
+
+scrollableElement1.addEventListener('scroll', function(){
+    if(scrollableElement1.scrollLeft === scrollableElement1.scrollWidth - scrollableElement1.clientWidth){
+        timeout = setTimeout(scrollback1, 6000)
+        clearInterval(myinterval1)
+    }
+})
+
+scrollableElement1.addEventListener('scroll', function(){
+    if(scrollableElement1.scrollLeft === 0){
+        myinterval1 = setInterval(scroll1, 6000)
+        clearTimeout(timeout)
+    }
+})
+
+timeout = setTimeout(scrollback1, 0)
+
+dot1.addEventListener('click', function(){
+    clearInterval(myinterval1)
+    clearTimeout(timeout)
+    timeout = setTimeout(scrollback1, 0)
+})
+
+dot2.addEventListener('click', function(){
+    clearInterval(myinterval1)
+    clearTimeout(timeout)   
+    myinterval1 = setInterval(scroll1, 0)
+})
